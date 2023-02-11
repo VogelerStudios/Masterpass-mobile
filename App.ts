2eaea85cdb6d608b4b744565da25d26bf7754dfd
hector <hpulido1021@gmail.com>
2023-02-11 01:48:32 -0500

    [ts-migrate][.] Run TS Migrate
    Co-authored-by: ts-migrate <>

--- App.ts
+++ App.ts
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+// @ts-expect-error TS(7016): Could not find a declaration file for module 'reac... Remove this comment to see the full error message
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    // @ts-expect-error TS(2552): Cannot find name 'style'. Did you mean 'styles'?
     <View style={styles.container}>
+      // @ts-expect-error TS(7027): Unreachable code detected.
       <Text>Open up App.js to start working on your app!</Text>
+      // @ts-expect-error TS(2552): Cannot find name 'style'. Did you mean 'styles'?
       <StatusBar style="auto" />
     </View>
   );
