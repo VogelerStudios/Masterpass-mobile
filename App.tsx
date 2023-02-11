b52b305f130b54925a4613cadf3c0e437fab03fe
Carlos <clover@Carloss-MBP.attlocal.net>
2023-02-11 02:46:22 -0500

    Added Urbanist Google font

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts, Urbanist_700Bold } from "@expo-google-fonts/urbanist";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  // Reference: https://www.npmjs.com/package/@expo-google-fonts/urbanist
+  let [fontsLoaded] = useFonts({
+    Urbanist_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  <View style={styles.container}>
+    <Text>Open up App.tsx to start working on your app!</Text>
+    <StatusBar style="auto" />
+  </View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    fontFamily: "Urbanist_700Bold",
   },
 });
